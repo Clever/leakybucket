a6fc0b5c1cbbf1ac2724e1deac4b7ba0299b2917
Rafael Garcia <rafael.garcia@clever.com>
2014-03-17 15:10:46 -0700

    bucket interface

--- bucket.go
+++ bucket.go
@@ -1,1 +1,25 @@
 package leakybucket
+
+import (
+	"time"
+)
+
+// Bucket interface for interacting with leaky buckets: https://en.wikipedia.org/wiki/Leaky_bucket
+type Bucket interface {
+	// Capcity of the bucket.
+	Capacity() uint
+
+	// Remaining space in the bucket.
+	Remaining() uint
+
+	// Reset returns when the bucket will be drained.
+	Reset() time.Time
+
+	// Add to the bucket.
+	Add(uint) error
+}
+
+// BucketFactory interface for generating buckets keyed by a string.
+type BucketFactory interface {
+	Create(string) Bucket
+}
